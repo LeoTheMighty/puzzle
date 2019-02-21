0e7f4290640594b17f12e82e4a6312d2943c6f5c
Leo Belyi <leonid@ac93.org>
2019-02-21 12:45:41 -0500

    Updates

---
+++ precache-manifest.29669b40cc11df68ac5d0b1b7793db75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c94f4b28ba89f3ce4ea3",
+    "url": "/puzzle/static/css/main.fd4bde79.chunk.css"
+  },
+  {
+    "revision": "c94f4b28ba89f3ce4ea3",
+    "url": "/puzzle/static/js/main.c94f4b28.chunk.js"
+  },
+  {
+    "revision": "550f34a01c447a11a5e7",
+    "url": "/puzzle/static/js/runtime~main.550f34a0.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/puzzle/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/puzzle/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5546793c3fb10609bfd9c8b4122b46cb",
+    "url": "/puzzle/index.html"
+  }
+];
